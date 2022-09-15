9bfed62840c84ffd231c01bb20987b65a443e640
Renz Lester J. Halagao <halagao72@gmail.com>
2022-09-15 10:22:15 +0800

    Implemented accordion in one card

---
+++ myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.moreDescription').hide();
+    $(".cards").click(function(){
+        var card = $('.moreDescription').attr('id')+"p .moreDescription";
+        // $('#moreDescriptionPayInFull').toggle();
+        // $('#showMoreDescription').toggleClass('showReverse');
+    });
+  });
