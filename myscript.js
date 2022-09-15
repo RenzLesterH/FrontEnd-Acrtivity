b24f1d8ff96ddd88840c6796e33b85d967960c35
Renz Lester J. Halagao <halagao72@gmail.com>
2022-09-15 11:01:02 +0800

    All cards accordion feature is successfully implemented.

--- myscript.js
+++ myscript.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.moreDescription').hide();
     $(".cards").click(function(){
-        var card = $('.moreDescription').attr('id')+"p .moreDescription";
-        // $('#moreDescriptionPayInFull').toggle();
-        // $('#showMoreDescription').toggleClass('showReverse');
+        var card = $(".moreDescription", this).attr('id');
+        $('#'+card+'').toggle();
+        var arrow = $(".show", this).attr('id');
+        $('#'+arrow+'').toggleClass('showReverse');
     });
   });
