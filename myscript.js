48caa866e1802efdddf5cb61c3055c7a5144f01e
Renz Lester J. Halagao <halagao72@gmail.com>
2022-09-15 11:25:48 +0800

    cards container padding bottom and height is adjusted.

--- myscript.js
+++ myscript.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.moreDescription').hide();
     $(".cards").click(function(){
         var card = $(".moreDescription", this).attr('id');
-        $('#'+card+'').toggle();
         var arrow = $(".show", this).attr('id');
+        $('#'+card+'').toggle();
         $('#'+arrow+'').toggleClass('showReverse');
     });
   });
